364282378fd2a8199c660a266c42ae5c67319b49
Sukh S <sukh.chauhan@gmail.com>
2022-03-30 22:53:05 -0400

    refactor web

---
+++ js/footer.js
@@ -0,0 +1,43 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="gap-top bounceInUp animated container">
+            <div class="box square charcoal">
+                <div class="container padded">
+                <div class="row">
+                    <div class="one small-tablet fourth padded">
+                    <h5 class="green">About NetR</h5>
+                    <ul class="unstyled">
+                        <li><a href="vision.html">Corporate Vision</a></li>
+                        <li><a href="arch.html">Products and Solutions</a></li>
+                        <li><a href="arch.html">Case Studies</a></li>
+                    </ul>
+                    </div>
+                    <div class="three small-tablet fourths padded">
+                    <h5 class="blue">Discover</h5>
+                    <ul class="unstyled three-column two-column-mobile">
+                        <li><a href="arch.html" title="Responsive grid system, grid adapters and helpers">Projects</a></li>
+                    </ul>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="box square">
+                <div class="container padded">
+                <div class="row">
+                    <div align="center">
+                    &copy; NetR Inc. 2022
+                    </div>
+                </div>
+                </div>
+            </div>
+        </footer>
+      `;
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
---
+++ js/header.js
@@ -0,0 +1,50 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <style type="text/css">
+            .logo {
+                position: relative;
+                top: -0.5em;
+            }
+            .logo a, .logo a:visited {
+                text-decoration: none;
+                color: #2B2B2D;
+            }
+            .logo img {
+                height: 2em;
+                position: relative;
+                top: 0.55em;
+                margin-right: 0.3em;
+            }
+        </style>
+        <header class="padded">
+            <div class="container">
+                <div class="row">
+                    <div class="one half">
+                        <h2 class="logo"><a href="/" target="_parent"><img src="images/logo_netr_box.png" alt="NetR"></a></h2>
+                    </div>
+                </div>
+                <nav role="navigation" class="nav gap-top blue">
+                    <ul role="menubar">
+                        <li><a href="index.html"><i class="icon-home"></i> Home</a></li>
+                        <li role="menu">
+                            <button>Services</button>
+                            <ul>
+                                <li><a href="arch.html">Architecture</a></li>
+                                <li><a href="dev.html">Development</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.html"> Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
